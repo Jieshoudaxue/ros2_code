5ea22b8c4731a6eb7e534cf4049e0c8b288d367f
jieshoudaxue <1641395022@qq.com>
2024-02-28 07:13:38 +0530

    [feat] add server client diy cpp

---
+++ service_cpp/src/server_diy.cpp
@@ -0,0 +1,31 @@
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "diy_interface/srv/question_and_answer.hpp"
+
+class ServiceServer : public rclcpp::Node 
+{
+public:
+    ServiceServer() : Node("service_server") {
+        server_ = this->create_service<diy_interface::srv::QuestionAndAnswer>("question_and_answer", 
+            std::bind(&ServiceServer::add_callback, this, std::placeholders::_1, std::placeholders::_2));
+    }
+private:
+    void add_callback(const std::shared_ptr<diy_interface::srv::QuestionAndAnswer::Request> request,
+                    std::shared_ptr<diy_interface::srv::QuestionAndAnswer::Response> response) {
+        response->answer = "I'm fine, thank you.";
+        RCLCPP_INFO(this->get_logger(), "[cpp server] receive: %s, return: %s", 
+                                        request->question.c_str(), response->answer.c_str());
+    }
+
+private:
+    rclcpp::Service<diy_interface::srv::QuestionAndAnswer>::SharedPtr server_;
+};
+
+int main(int argc, char * argv[]) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<ServiceServer>());
+    rclcpp::shutdown();
+
+    return 0;
+}
